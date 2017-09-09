575c125c1893ee4a5f1eb7633fb11d6092ef936c
Micah Bell <redfroglet@gmail.com>
2017-09-09 15:35:53 +0900

    Sept 9 2017

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-var express = require('express');
-var app = express();
+const http = require('http');
+const server = http.createServer();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+server.on('request', (request, response) => {
+  // the same kind of magic happens here!
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
